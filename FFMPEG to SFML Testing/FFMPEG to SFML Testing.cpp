c68bdb9c65767d87ce63adcd7a1191e7cc2b8627
xzips <aspenxe@gmail.com>
2022-10-29 02:21:36 -0400

    add more code probablly

--- FFMPEG to SFML Testing/FFMPEG to SFML Testing.cpp
+++ FFMPEG to SFML Testing/FFMPEG to SFML Testing.cpp
@@ -60,32 +60,25 @@ using std::chrono::milliseconds;
 
 
 
-int main(int argc, char** argv)
-{
+void BmpFileDemo() {
     //const char bmp_path[] = "C:/Users/Aspen/Desktop/XTracker/bmp_sample.bmp";
     const char bmp_path[] = "C:/Users/aspen/Desktop/XTracker/ffmpeg/bin/output1.bmp";
 
 
-
-
-   
-    
-
     sf::Vector2i img_dims = GetBmpDims(bmp_path);
     size_t img_pix_count = img_dims.x * img_dims.y;
-    
+
     unsigned char* loaded_bmp_rgba = (unsigned char*)malloc(img_pix_count * 4);
 
     if (loaded_bmp_rgba == nullptr) throw "Primary Pixel Array Malloc Failed";
     auto t1 = high_resolution_clock::now();
     ReadBMP_To_RGBA_Ptr(loaded_bmp_rgba, bmp_path, 0);
 
-    
+
     auto t2 = high_resolution_clock::now();
     auto ms_int = duration_cast<milliseconds>(t2 - t1);
     duration<double, std::milli> ms_double = t2 - t1;
     std::cout << ms_double.count() << "ms for 1 frame\n";
-
 
 
     sf::Uint8* bmp_sf_pixels = loaded_bmp_rgba;
@@ -94,15 +87,11 @@ int main(int argc, char** argv)
     bmp_tex.create(img_dims.x, img_dims.y);
     bmp_tex.update(bmp_sf_pixels);
 
-    
-
-   
 
     sf::Sprite bmp_sprite;
     bmp_sprite.setTexture(bmp_tex);
 
     bmp_sprite.setScale(0.6, 0.6);
-
 
 
     sf::RenderWindow window(sf::VideoMode(800, 600), "XTracker - Optimized");
@@ -130,7 +119,116 @@ int main(int argc, char** argv)
     }
 
     free(loaded_bmp_rgba);
+}
+
+
+int main(int argc, char** argv)
+{
+   // BmpFileDemo();
+    //ffmpeg -i earth_video.mp4 -vframes 1 -c:v rawvideo -f image2pipe -
+    // 
+    //ffmpeg -i earth_video.mp4 -vframes 1 -pix_fmt rgb32 -f image2pipe -
+
+    FILE* fp;
+ 
+    size_t img_size_memory = 1280 * 720 * 4;
+
+    char* pix_ptr = (char*)malloc(img_size_memory);
+
+
+    std::string full_cmd = "C:/Users/aspen/Desktop/XTracker/ffmpeg/bin/"
+         "ffmpeg -i C:/Users/aspen/Desktop/XTracker/ffmpeg/bin/earth_video.mp4 -vframes 1 -pix_fmt rgb24 -f image2pipe -";
+
+    fp = _popen(full_cmd.c_str(), "r");
+    //if (fp == 0) exit(-1);
+
+
+    char pix_buffer[4];
+
+
+    //char c;
+   // while (std::cin >> c)
+   // {
+     //   std::cout << "Pixel: " << (uint32_t)(uint8_t)c << "\n";
+    //}
+    
+    size_t pix_c = 0;
+    while (fgets(pix_buffer, 4, fp))
+    {
+        pix_ptr[pix_c * 4] = pix_buffer[0];
+        pix_ptr[pix_c * 4+1] = pix_buffer[1];
+        pix_ptr[pix_c * 4+2] = pix_buffer[2];
+        pix_ptr[pix_c * 4+3] = pix_buffer[3];
+        std::cout << "Pixel "<< pix_c << ": " << (uint32_t)(uint8_t)pix_buffer[0] << "\n";
+        pix_c++;
+    }
+      
+
+
+    //_pclose(fp);
+
+
+
+
+
+
+    
+    sf::Vector2i img_dims = sf::Vector2i(1280, 720);
+    size_t img_pix_count = img_dims.x * img_dims.y;
+
+    //unsigned char* loaded_bmp_rgba = (unsigned char*)malloc(img_pix_count * 4);
+
+    //if (loaded_bmp_rgba == nullptr) throw "Primary Pixel Array Malloc Failed";
+   // auto t1 = high_resolution_clock::now();
+    //ReadBMP_To_RGBA_Ptr(loaded_bmp_rgba, bmp_path, 0);
+    /*
+
+    auto t2 = high_resolution_clock::now();
+    auto ms_int = duration_cast<milliseconds>(t2 - t1);
+    duration<double, std::milli> ms_double = t2 - t1;
+    std::cout << ms_double.count() << "ms for 1 frame\n";
+    */
+
+    sf::Uint8* bmp_sf_pixels = (sf::Uint8*)pix_ptr;
+
+    sf::Texture bmp_tex;
+    bmp_tex.create(img_dims.x, img_dims.y);
+    bmp_tex.update(bmp_sf_pixels);
+
+
+    sf::Sprite bmp_sprite;
+    bmp_sprite.setTexture(bmp_tex);
+
+    bmp_sprite.setScale(0.6, 0.6);
+
+
+    sf::RenderWindow window(sf::VideoMode(800, 600), "XTracker - Optimized");
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+            if (event.type == sf::Event::KeyPressed)
+            {
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+                {
+                    window.close();
+                }
+            }
+        }
+
+
+        window.clear();
+        window.draw(bmp_sprite);
+        window.display();
+    }
+
+   // free(loaded_bmp_rgba);
+
+   
 
     return 0;
-
 }
--- Video Object Tracker Optimized/ImageAnalysis.h
+++ Video Object Tracker Optimized/ImageAnalysis.h
@@ -1,8 +1,114 @@
 #pragma once
+#include <SFML/Graphics.hpp>
 
-double difference_metric_at_pos(sf::Image& base_image, sf::Image& compare_template,
-    uint32_t center_x, uint32_t center_y, uint32_t rect_width, uint32_t rect_height)
+
+
+sf::Image extract_rectangle_as_image(const sf::Texture& tex, size_t center_x, size_t center_y, size_t rect_width, size_t rect_height)
 {
+    sf::Image extracted_image;
+    sf::Image full_image = tex.copyToImage();
+
+    //fix this lol
+    center_x -= rect_width >> 1;
+    center_y -= rect_height >> 1;
+
+
+    //check this
+    size_t extracted_img_buff_sz = sizeof(sf::Uint8) * rect_width * rect_height * 4;
+    sf::Uint8* extracted_image_pixels = (sf::Uint8*)malloc(extracted_img_buff_sz);
+    if (extracted_image_pixels == nullptr)
+    {
+        std::cout << "somebody(I) fucked up memory management and now we can't have nice things, contact the dev" << std::endl;
+    }
+
+
+
+    const sf::Uint8* original_image_p_buffer = full_image.getPixelsPtr();
+
+    size_t cur_loc_in_extr_pix = 0;
+
+    size_t numPixels = full_image.getSize().x * full_image.getSize().y * 4;
+    //for each row, find out where the fuck we are and memcpy pixels 
+    for (size_t row = 0; row < rect_height; ++row)
+    {
+        size_t row_begin_indx = ((row + center_y) * tex.getSize().x + center_x) * 4;
+
+
+        sf::Uint8* dst_addr = extracted_image_pixels + cur_loc_in_extr_pix;
+
+        if (dst_addr == nullptr) exit(-1);
+
+        memcpy(dst_addr, original_image_p_buffer + row_begin_indx, 4 * rect_width);
+
+        cur_loc_in_extr_pix += rect_width * 4;
+    }
+
+    extracted_image.create(rect_width, rect_height, extracted_image_pixels);
+    free(extracted_image_pixels);
+
+    return extracted_image;
+
+}
+
+
+//NO ERROR CHECKING, INPUTS BETTER BE VALID!!! :(
+double difference_metric_at_pos(const sf::Uint8 *base_pixels_ptr, const sf::Uint8 *template_pixels_ptr,
+    const uint32_t center_x, const uint32_t center_y, const uint32_t rect_width, const uint32_t rect_height,
+    const uint32_t base_width, const uint32_t base_height)
+{
+    //for each row, find out where the fuck we are and compare pixels 
+    for (size_t rect_row = 0; rect_row < rect_height; ++rect_row)
+    {
+        for (size_t rect_col = 0; rect_col < rect_width; rect_col++)
+        {
+            const size_t base_row_indx = (rect_row + center_y) - (rect_width >> 1);
+            
+            const size_t base_pixels_px_index = (rect_col * 4) + base_row_indx * base_width * 4;
+
+            const size_t template_pixels_px_index = (rect_col * 4) + rect_row * rect_width * 4;;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                
+
+
+
+        }
+     
+    }
+
+
+
+
+
+
+
+
+
+
+
     //return compare_template;
     size_t compare_imgs_sz = sizeof(sf::Uint8) * compare_template.getSize().x * compare_template.getSize().y * 4;
 
